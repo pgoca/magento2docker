b81dfb98bf182c5033ad4b127d2f7ef698fcee21
Szymon Łuczak <sluczak@kaliop.com>
2018-11-13 13:14:54 +0100

    default magento files added

---
+++ build/magento/env.php
@@ -0,0 +1,43 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => '7cebd0a1fa6509473e45772ba709a1cf',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'port' => '13{{PROJECT_NUMBER}}',
+        'dbname' => '{{DATABASE_NAME}}',
+        'username' => 'root',
+        'password' => 'root',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+);
